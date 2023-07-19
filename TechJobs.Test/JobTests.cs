910e293d166b7d2f7c83ffa225e5c244276acfcd
Cory <corydeuschle92@gmail.com>
2023-07-19 01:41:13 -0500

    Completed all parts, few tests need to be tinkered with still

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -1,17 +1,81 @@
 ﻿
+using System.Data;
+using System.Xml.Linq;
+
 namespace TechJobs.Tests
 {
 	[TestClass]
 	public class JobTests
 	{
         //Testing Objects
-        //initalize your testing objects here
+        Job job1 = new Job();
+
+        Job job2 = new Job();
+
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new
+        PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new 
+        PositionType("Quality control"), new CoreCompetency("Persistence"));
 
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobId()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            Assert.AreEqual(job1.Id, job2.Id - 1 );
         }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Assert.AreEqual("Product tester", job3.Name );
+            Assert.AreEqual("ACME", job3.EmployerName.Value );
+            Assert.AreEqual("Desert", job3.EmployerLocation.Value );
+            Assert.AreEqual("Quality control", job3.JobType.Value );
+            Assert.AreEqual("Persistence", job3.JobCoreCompetency.Value );
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Assert.IsFalse( job1.Equals( job2 ) );
+        }
+
+        [TestMethod]
+        public void TestToStringStartsAndEndsWithNewLine()
+        {
+            string layout =
+                "\n" +
+                "\n";
+
+            string correctLayout = job3.ToString();
+
+            Assert.AreEqual( layout, correctLayout );
+        }
+
+        [TestMethod]
+        public void TestToStringContainsCorrectLabelsAndData()
+        {
+            string correctLayout =
+            $"\n" +
+                    $"ID: {job3.Id}\n" +
+                    $"Name: {job3.Name}\n" +
+                    $"Employer: {job3.EmployerName.Value}\n" +
+            $"Location: {job3.EmployerLocation.Value}\n" +
+                    $"Position Type: {job3.JobType.Value}\n" +
+                    $"Core Competency: {job3.JobCoreCompetency.Value}\n" +
+                    $"\n";
+
+            string layoutToTest = job3.ToString();
+
+            Assert.AreEqual( layoutToTest, correctLayout );
+        }
+
+        [TestMethod]
+        public void TestToStringHandlesEmptyField()
+        {
+            Assert.AreEqual("Data not available", job1.ToString());
+        }
+
     }
 }
 
--- TechJobsOOAutoGraded6/Job.cs
+++ TechJobsOOAutoGraded6/Job.cs
@@ -3,7 +3,7 @@ namespace TechJobsOOAutoGraded6
 {
 	public class Job
 	{
-        /*
+        
 
             public int Id { get; }
             private static int nextId = 1;
@@ -15,13 +15,59 @@ namespace TechJobsOOAutoGraded6
 
             // TODO: Task 3: Add the two necessary constructors.
 
-            // TODO: Task 3: Generate Equals() and GetHashCode() methods.  
+            public Job()
+            {
+                Id = nextId;
+                nextId++;
+            }
 
-            // TODO: Task 5: Generate custom ToString() method.
-                //Until you create this method, you will not be able to print a job to the console.
+            public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+            {
+                Name = name;
+                EmployerName = employerName;
+                EmployerLocation = employerLocation;
+                JobType = jobType;
+                JobCoreCompetency = jobCoreCompetency;
+            }
 
 
-        */
+        // TODO: Task 3: Generate Equals() and GetHashCode() methods.  
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        // TODO: Task 5: Generate custom ToString() method.
+        public override string ToString()
+        {
+            if (Name == null || EmployerName == null || EmployerLocation == null || JobType == null || JobCoreCompetency == null)
+            {
+                return "Data not available";
+            }
+            else
+            {
+                return $"\n" +
+                    $"ID: {Id}\n" +
+                    $"Name: {Name}\n" +
+                    $"Employer: {EmployerName.Value}\n" +
+                    $"Location: {EmployerLocation.Value}\n" +
+                    $"Position Type: {JobType.Value}\n" +
+                    $"Core Competency: {JobCoreCompetency.Value}\n" +
+                    $"\n";
+            }
+        }
+
+        //Until you create this method, you will not be able to print a job to the console.
+
+
+
     }
 }
 
